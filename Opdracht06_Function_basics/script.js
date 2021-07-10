088a33675237b2781cb730e549a8096de7be55f4
junerae <80211043+junerae@users.noreply.github.com>
2021-07-10 18:43:14 +0200

    Add files via upload

---
+++ Opdracht06_Function_basics/script.js
@@ -0,0 +1,31 @@
+console.log("Make sushi!");
+
+const makeSushi = function () {
+    console.log("Prep the Rice & Nori. Lay out the nori on the bamboo mat");
+    console.log("Spread the Rice on the Nori");
+    console.log("Add Fillings to the Bottom Quarter of the Nori");
+    console.log("Roll the Sushi");
+    console.log("Cut the Sushi");
+    console.log("Serve");
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
+
+
+
+console.log("How to make pizza");
+
+const makePizza = function () {
+    console.log("Make the dough and shape it");
+    console.log("Cover the dough in plastic wrap and wait until you can use it");
+    console.log("Shape the dough");
+    console.log("Cover the pizza dough with tomato sauce and cheese");
+    console.log("Add toppings");
+    console.log("Bake for 25 minutes in oven");
+}
+
+makePizza();
+makePizza();
+
